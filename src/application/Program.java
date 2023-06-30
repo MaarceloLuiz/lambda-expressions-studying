92e10417e94d5bc4acd3d377b093fea151002fbe
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-06-30 12:19:47 +0100

    Anonymous Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -16,8 +16,17 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		//the method sort receive a "Comparator" as an argument
-		list.sort(new MyComparator());
+		//creating an Anonymous Class
+		
+		//we don't necessarily need a separated class to implement the Comparator method, we can create this anonymous class on the main Program
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+
+		list.sort(comp);
 
 		for (Product p : list) {
 			System.out.println(p);
